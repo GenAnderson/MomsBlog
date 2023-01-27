184724a447e935e2561637443fcdc4c445c06d94
Genevieve Anderson <114549536+GenAnderson@users.noreply.github.com>
2023-01-26 15:49:52 -1000

    change home layout

--- components/heroSlider.tsx
+++ components/heroSlider.tsx
@@ -9,8 +9,8 @@ const HeroSlider = function () {
     "slide1.jpg",
     "slide2.jpg",
     "slide3.jpg",
-    "slide4.jpg",
-    "slide5.jpg",
+    // "slide4.jpg",
+    // "slide5.jpg",
   ];
 
   useEffect(() => {
--- components/marketing.tsx
+++ components/marketing.tsx
@@ -3,6 +3,10 @@ import { useInView } from "react-intersection-observer";
 import styles from "./allComponents.module.scss";
 
 const Marketing: any = () => {
+  const { ref: refMarketMiddle, inView: middleInView } = useInView({
+    triggerOnce: true,
+  });
+
   const { ref: refMarket1, inView: market1InView } = useInView({
     triggerOnce: true,
   });
@@ -27,20 +31,42 @@ const Marketing: any = () => {
     <div className={styles.marketingContainer}>
       <div className={styles.controlledVHcontainer}>
         <div className={styles.marketingTextBox}>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum,
-            optio quod, deserunt laudantium iste tenetur et voluptas, vel totam
-            nobis quisquam! Alias autem delectveniam magni corrupti consequatur
-            soluta praesentium molestiae omnis ratione nesciunt adipisci dicta
-            hic est corporis sapiente, veritatis fugiat eos?
-          </p>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum,
-            optio quod, deserunt laudantium iste tenetur et voluptas, vel totam
-            nobis quisquam! Alias autem delectus.
+          <img src="./flower4.jpg" className={styles.marketingImg} />
+          <div>
+            <p className={styles.miniHeader}>
+              <em>Aloha, I'm Lujean</em>
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum,
+              optio quod, deserunt laudantium iste tenetur et voluptas, vel
+              totam nobis quisquam! Alias autem delectveniam magni corrupti
+              consequatur soluta praesentium molestiae omnis ratione nesciunt
+              adipisci dicta hic est corporis sapiente, veritatis fugiat eos?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorum,
+              optio quod, deserunt laudantium iste tenetur et voluptas, vel
+              totam nobis quisquam! Alias autem delectus.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.marketBoxMiddleSection}>
+        <div
+          ref={refMarketMiddle}
+          className={`${styles.middleSectionMvmtContainer} ${
+            middleInView ? styles.middleInView : ""
+          }`}
+        >
+          <p className={styles.miniHeader}>Heading</p>
+          <p className={styles.middleSectionText}>
+            Something important or highlight something here. Could be your
+            favorite quote, or mantra, etc?
           </p>
         </div>
       </div>
+
       <div className={styles.marketBoxMiniContainer}>
         <div
           ref={refMarket1}
@@ -48,6 +74,11 @@ const Marketing: any = () => {
             market1InView ? styles.market1 : ""
           }`}
         >
+          <img
+            src="./icon_listen.png"
+            className={styles.marketIcons}
+            alt="listen icon"
+          />
           Building rapport through listening for understanding and validation
         </div>
         <div
@@ -56,6 +87,11 @@ const Marketing: any = () => {
             market2InView ? styles.market2 : ""
           }`}
         >
+          <img
+            src="./icon_cognitive.png"
+            className={styles.marketIcons}
+            alt="cognitive icon"
+          />
           Cognitive Behavioral Therapy to identify and modify thinking errors.
         </div>
         <div
@@ -64,6 +100,11 @@ const Marketing: any = () => {
             market3InView ? styles.market3 : ""
           }`}
         >
+          <img
+            src="./icon_interview.png"
+            className={styles.marketIcons}
+            alt="interview icon"
+          />
           Solution Focused Interviewing
         </div>
         <div
@@ -72,6 +113,11 @@ const Marketing: any = () => {
             market4InView ? styles.market4 : ""
           }`}
         >
+          <img
+            src="./icon_support.png"
+            className={styles.marketIcons}
+            alt="support icon"
+          />
           Strength Based Therapy to identify strengths and support systems
           clients already have.
         </div>
@@ -81,12 +127,15 @@ const Marketing: any = () => {
             market5InView ? styles.market5 : ""
           }`}
         >
+          <img
+            src="./icon_confidence.png"
+            className={styles.marketIcons}
+            alt="confidence icon"
+          />
           Assertiveness Training to build self-confidence and problem-solving
           skills.
         </div>
       </div>
-
-      {/* <img src="./rose.png" className={styles.marketImg__2} /> */}
     </div>
   );
 };
